6bffaa5a8349c5ab5a3135131dc37729b0038ab5
Logan-Lim <Logan.Lim@cra-arc.gc.ca>
2020-01-13 12:02:19 -0500

    add dynamic backend url

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         new webpack.DefinePlugin({
             // global app config object
             config: JSON.stringify({
-                apiUrl: 'http://localhost:3000'
+                apiUrl: process.env.BACKEND_URL || 'http://localhost:3000'
             })
         })
     ],
